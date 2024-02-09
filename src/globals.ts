95a8229c8a674b0c63a8cba0141ad9772768323d
Riley <waskerdu@gmail.com>
2024-02-08 21:25:18 -0800

    gamepad input

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
-import { InputManager, Key } from "./cleo/core/input_manager";
+import { InputManager, JoyAxis, JoyButton, Key } from "./cleo/core/input_manager";
 import { TextureManager } from "./cleo/core/texture_manager";
 
 export class Globals{
@@ -13,13 +13,19 @@ export class Globals{
             .addKeyNegative(Key.a)
             .addKeyNegative(Key.left)
             .addKeyPositive(Key.d)
-            .addKeyPositive(Key.right);
+            .addKeyPositive(Key.right)
+            .addJoyAxis(0, JoyAxis.lx)
+            .addJoyButtonNegative(0, JoyButton.d_left)
+            .addJoyButtonPositive(0, JoyButton.d_right);
         moveAxis.yAxis
             .addKeyNegative(Key.w)
             .addKeyNegative(Key.up)
             .addKeyPositive(Key.s)
-            .addKeyPositive(Key.down);
-        this.inputManager.addButton("jump").addKey(Key.space);
+            .addKeyPositive(Key.down)
+            .addJoyAxis(0, JoyAxis.ly)
+            .addJoyButtonNegative(0, JoyButton.d_up)
+            .addJoyButtonPositive(0, JoyButton.d_down);
+        this.inputManager.addButton("jump").addKey(Key.space).addJoyButton(0, JoyButton.a);
         this.textureManager = new TextureManager()
             .add("characters", "./sprites/tilemap-characters_packed.png")
             .add("tiles", "./sprites/tilemap_packed.png")
